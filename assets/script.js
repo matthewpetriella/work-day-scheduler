88388de1b5fba16a8064b9807eecd21b3184595e
Matthew Petriella <petriellaproductions@gmail.com>
2022-04-27 20:06:37 -0400

    updated time blocks based on stack overflow answer

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ $(document).ready( function() {
 });
 
 // function to change color of text area based on past, present, future
+//source https://stackoverflow.com/questions/59995703/trying-to-change-background-color-based-off-if-the-hour-is-in-the-past-current
 function colorChange () {
     
     var currentTime = moment().hours();
@@ -47,43 +48,3 @@ $(".saveBtn").click(function() {
     renderText ();
     
 });
-
-    // get and set event text input
-function renderText () {
-
-    var saveEventText9 = JSON.parse(localStorage.getItem("9:00 am"));
-    $("#9").val("");
-    $("#9").val(saveEventText9);
-    
-    var saveEventText10 = JSON.parse(localStorage.getItem("10:00 am"));
-    $("#10").val("");
-    $("#10").val(saveEventText10);
-    
-    var saveEventText11 = JSON.parse(localStorage.getItem("11:00 am"));
-    $("#11").val("");
-    $("#11").val(saveEventText11);
-    
-    var saveEventText12 = JSON.parse(localStorage.getItem("12:00 pm"));
-    $("#12").val("");
-    $("#12").val(saveEventText12);
-    
-    var saveEventText1 = JSON.parse(localStorage.getItem("1:00 pm"));
-    $("#13").val("");
-    $("#13").val(saveEventText1);
-
-    var saveEventText2 = JSON.parse(localStorage.getItem("2:00 pm"));
-    $("#14").val("");
-    $("#14").val(saveEventText2);
-
-    var saveEventText3 = JSON.parse(localStorage.getItem("3:00 pm"));
-    $("#15").val("");
-    $("#15").val(saveEventText3);
-
-    var saveEventText4 = JSON.parse(localStorage.getItem("4:00 pm"));
-    $("#16").val("");
-    $("#16").val(saveEventText4);
-
-    var saveEventText5 = JSON.parse(localStorage.getItem("5:00 pm"));
-    $("#17").val("");
-    $("#17").val(saveEventText5);
-};
